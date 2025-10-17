15cca84d6af9b00c08df4d55b13efa3902a12618
ebotfabien <touchone0001@gmail.com>
2025-10-17 19:08:54 +0200

    fixed_ok_2

--- house_prices/inference.py
+++ house_prices/inference.py
@@ -54,5 +54,5 @@ def make_predictions(inference_df: pd.DataFrame) -> pd.Series:
 
     # --- Predict using loaded model
     predictions = linear_regression_model_loaded.predict(X_infer_final_df.values)
-
+    #add fixes
     return predictions
